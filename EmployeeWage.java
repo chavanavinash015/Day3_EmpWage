b37335cbca87aca83f31e0c5788c9670ce4f0662
arc-015 <chavanavinash015@gmail.com>
2022-10-13 23:49:45 +0530

    Employee switch case

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -12,15 +12,19 @@ public class EmployeeWage {
 
         int dailyWage = 0;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee Full Time");
-            dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-        } else if(empCheck == IS_PART_TIME){
-            System.out.println("Employee Part Time");
-            dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-        }else {
-            System.out.println("Employee Absent");
-        }
+
+       switch(empCheck) {
+           case IS_FULL_TIME:
+               System.out.println("Employee Full Time");
+               dailyWage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+               break;
+           case IS_PART_TIME:
+               System.out.println("Employee Part Time");
+               dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+               break;
+           default:
+               System.out.println("Employee Absent");
+       }
         System.out.println("Daily Wage => " + dailyWage);
     }
 }
